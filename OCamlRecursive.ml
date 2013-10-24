5ba581ccef8bbb784cd70a2663bcad0d3e210101
Haruto <harold.fong@epita.fr>
2013-10-24 19:17:58 +0200

    1er soutenance

--- OCamlRecursive.ml
+++ OCamlRecursive.ml
@@ -1,6 +1,7 @@
 (* Dimensions d'une image *)
 let get_dims img =
-  ((Sdlvideo.surface_info img).Sdlvideo.w, (Sdlvideo.surface_info img).Sdlvideo.h)
+  ((Sdlvideo.surface_info img).Sdlvideo.w, 
+   (Sdlvideo.surface_info img).Sdlvideo.h)
 
  (* main *)
 let main () =
@@ -14,18 +15,19 @@ let main () =
     let img = Sdlloader.load_image Sys.argv.(1) in
     (* On récupère les dimensions *)
     let (w,h) = get_dims img in
-    (* On crée la surface d'affichage en doublebuffering *)
-    let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] and 
-	dst = Sdlvideo.create_RGB_surface_format img [] w h in
+    (* On crée la surface d'arriver pour la binarisation *)
+   let bin = Sdlvideo.create_RGB_surface_format img [] w h in
     (* Application des differentes fonctions de traitement *)
-    Binarisation.binarisation img dst w h;
-    Segmentation.get_text_zone dst w h;
-    (* on affiche l'image *)
-    Tools.show dst display;
-    (* on attend une touche *)
+    Binarisation.binarisation img bin w h;
+    let alpha = Rotation.(hough_accu bin) in
+    let (dst1,newW,newH) = Rotation.(rotate bin alpha) in
+    let dst_final = Sdlvideo.set_video_mode newW newH [`DOUBLEBUF] in
+    Tools.show dst1 dst_final;
     Tools.wait_key ();
-    (* Enregistrement de l'image en .BMP *)
-    Sdlvideo.save_BMP dst "output.bmp";
+    Segmentation.get_text_zone dst1 w h;
+    (* on affiche l'image *)
+    Tools.show dst1 dst_final;
+    Tools.wait_key ();
     (* on quitte *)
     exit 0
   end
