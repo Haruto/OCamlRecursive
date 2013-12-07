3f1235dc5f3bcc2f790a357c66e0ac1d23120a56
Haruto <harold.fong@epita.fr>
2013-12-07 20:25:23 +0100

    RdN

--- neuralnetwork.ml
+++ neuralnetwork.ml
@@ -1,13 +1,19 @@
 class neuron nbentries =
 object (self)
   val mutable weights:float array = 
-    Array.make (nbentries-1) (Random.float 2.)
+    Array.make (nbentries-1) (Random.float 0.5)
   val mutable output:float = 0.
   val threshold:float = 0.5
+  val mutable delta = 0.
 
   method sigmoidale x = 1. /. 1. +. (exp (-.x))
+
   method get_output = output
+  method get_weights pos = weights.(pos)
+  method get_delta = delta
+    
   method set_weights pos weight = weights.(pos) <- weight
+  method set_delta d = delta <- d
   method set_output tab_entries = 
       begin
 	for i = 0 to nbentries-1 do
@@ -50,11 +56,20 @@ object (self)
       )
       nbneurons_tab
   val threshold = 0.5
- 
+  val mutable delta = 0.
+  val mutable error = 0.
+
+  method get_layers = layers
+  method get_delta = delta
+  method get_error = error
+
+  method set_delta d = delta <- d
+  method set_error e = error <- e
   method set_layer pos l = layers.(pos) <- l
   method layer_update tab_entries = 
-    for i = 0 to (Array.length layers)-1 do
-      layers.(i)#set_output_tab tab_entries
+    layers.(0)#set_output_tab tab_entries;
+    for i = 1 to (Array.length layers)-1 do
+	layers.(i)#set_output_tab layers.(i-1)#get_output_neuron
     done
 end
-
+  
